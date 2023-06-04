df7d3f65d8082ce71c6bc26b27dee9f00586191d
Aman Jain <ajinhindauncity@gmail.com>
2023-06-04 16:54:09 +0530

    creating Popular Services component

--- client/src/components/Companies.jsx
+++ client/src/components/Companies.jsx
@@ -1,9 +1,17 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Companies() {
   return (
-    <div>
-      Companies
+    <div className='flex items-center justify-center text-2xl text-gray-400 font-bold min-h-[11vh]'>
+      Trusted by: &nbsp;
+      <ul className='flex gap-10 justify-center ml-10'>
+        {[1, 2, 3, 4, 5].map((num) => (
+          <li key={num} className='h-[4.5rem] w-[4.5rem] relative'>
+            <Image alt='trusted brands' fill src={`/trusted${num}.png`}/>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
