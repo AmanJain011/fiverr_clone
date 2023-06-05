32eff3ec87d8f4c95d1812026e9eb57786e3be66
Aman Jain <ajinhindauncity@gmail.com>
2023-06-05 16:00:58 +0530

    setup for footer component

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,9 +1,13 @@
+import Footer from '@/components/Footer'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return(
-    <div className='w-full mx-auto mb-auto'>
-      <Component {...pageProps} />
+  return (
+    <div>
+      <div className='w-full mx-auto mb-auto'>
+        <Component {...pageProps} />
+      </div>
+      <Footer />
     </div>
   )
 }
